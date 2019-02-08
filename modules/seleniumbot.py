24be5a653e85a412a0d2a4418fc69f2c18b6b318
Matteo Gaito <matteo.gaito@gmail.com>
2019-02-08 17:00:30 +0100

    added mouse

--- modules/seleniumbot.py
+++ modules/seleniumbot.py
@@ -13,6 +13,7 @@ from modules.storeusername import store
 from activate_account import get_activation_url
 # library import
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys  # and Krates
 import requests
 import re
@@ -42,38 +43,44 @@ class AccountCreator():
 
         driver = webdriver.Chrome(chrome_options=chrome_options)
         driver.get(self.url)
+        action_chains = ActionChains(driver)
         sleep(5)
         # username
         account_info = accnt.new_account()
 
         # fill the email value
         email_field = driver.find_element_by_name('emailOrPhone')
+        action_chains.move_to_element(email_field)
         email_field.send_keys(account_info["email"])
 
         sleep(2)
 
         # fill the fullname value
         fullname_field = driver.find_element_by_name('fullName')
+        action_chains.move_to_element(fullname_field)
         fullname_field.send_keys(account_info["name"])
 
         sleep(2)
 
         # fill username value
         username_field = driver.find_element_by_name('username')
+        action_chains.move_to_element(username_field)
         username_field.send_keys(account_info["username"])
 
         sleep(2)
 
         # fill password value
         password_field = driver.find_element_by_name('password')
+        action_chains.move_to_element(password_field)
         passW = account_info["password"]
         password_field.send_keys(passW)
 
         sleep(2)
 
-
         submit = driver.find_element_by_xpath(
             '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[7]/div/button')
+
+        action_chains.move_to_element(submit)
 
         sleep(2)
         submit.click()
@@ -85,7 +92,6 @@ class AccountCreator():
 
         sleep(2)
         next_button = driver.find_elements_by_xpath('//button[text()="Next"]')[1]
-        print(next_button)
         next_button.click()
 
         sleep(4)
@@ -96,8 +102,6 @@ class AccountCreator():
         confirm_url = get_activation_url(account_info['email'])
         logging.info("The confirm url is {}".format(confirm_url))
         driver.get(confirm_url)
-
-        sleep(100000)
 
         driver.close()
 
