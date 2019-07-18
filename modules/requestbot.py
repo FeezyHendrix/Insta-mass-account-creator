277bb57e3bbd3256ccd219141bad244f7597e3b8
feezyhendrix <switchdevelop1@gmail.com>
2019-07-18 23:00:11 +0100

    merged updates from master

--- modules/requestbot.py
+++ modules/requestbot.py
@@ -21,20 +21,21 @@ class CreateAccount:
         self.use_custom_proxy = use_custom_proxy
         self.use_local_ip_address = use_local_ip_address
         self.url = "https://www.instagram.com/accounts/web_create_ajax/"
-        self.headers = {
-            'accept': "*/*",
-            'accept-encoding': "gzip, deflate, br",
-            'accept-language': "en-US,en;q=0.8",
-            'content-length': "241",
-            'content-type': 'application/x-www-form-urlencoded',
-            'origin': "https://www.instagram.com",
-            'referer': "https://www.instagram.com/",
-            'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36",
-            'x-csrftoken': "95RtiLDyX9J6AcVz9jtUIySbwf75WhvG",
-            'x-instagram-ajax': "c7e210fa2eb7",
-            'x-requested-with': "XMLHttpRequest",
-            'Cache-Control': "no-cache",
-        }
+        self.referer_url = "https://www.instagram.com/"
+        # self.headers = {
+        #     'accept': "*/*",
+        #     'accept-encoding': "gzip, deflate, br",
+        #     'accept-language': "en-US,en;q=0.8",
+        #     'content-length': "241",
+        #     'content-type': 'application/x-www-form-urlencoded',
+        #     'origin': "https://www.instagram.com",
+        #     'referer': "https://www.instagram.com/",
+        #     'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36",
+        #     'x-csrftoken': "95RtiLDyX9J6AcVz9jtUIySbwf75WhvG",
+        #     'x-instagram-ajax': "c7e210fa2eb7",
+        #     'x-requested-with': "XMLHttpRequest",
+        #     'Cache-Control': "no-cache",
+        # }
         self.__collectcrsf()
         self.__collect_sockets()
 
@@ -46,23 +47,25 @@ class CreateAccount:
         for socket_str in revised_list:
             self.sockets.append(socket_str[:-5].replace("</td>", ":"))
 
-    def __collectcrsf(self):
-        r = requests.get('https://instagram.com/accounts/emailsignup/')
-        print(r)
+    # def __collectcrsf(self):
+    #     r = requests.get('https://instagram.com/accounts/emailsignup/')
+    #     print(r)
 
     # Account creation function
     def createaccount(self):
         # Account creation payload
+        session = requests.Session()
+        
+        try:
+            session_start = session.get(self.url)
         payload = {
             'email': self.email,
             'password': self.password,
             'username': self.username,
             'first_name': self.name,
-            'client_id': 'W6mHTAAEAAHsVu2N0wGEChTQpTfn',
-            'seamless_login_enabled': '1',
-            'gdpr_s': '%5B0%2C2%2C0%2Cnull%5D',
-            'tos_version': 'eu',
-            'opt_into_one_tap': 'false'
+           'seamless_login_enabled' : '1',
+            'tos_version' : 'row',
+            'opt_into_one_tap' : 'false'
         }
         if self.use_local_ip_address is True:
             request = requests.post(self.url, data=payload, headers=self.headers)
