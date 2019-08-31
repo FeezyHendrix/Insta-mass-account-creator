5c91a64dc7371060031476b728051ceeff5a7de4
Shubham <itseasy21@gmail.com>
2019-08-31 21:47:59 +0530

    Fix #83
    Fixed "can only concatenate str (not "RecursionError") to str" issue

--- modules/storeusername.py
+++ modules/storeusername.py
@@ -3,7 +3,7 @@ import logging
 import pickle
 
 def store(account):
-    with open(ASSET_DIR + '/usernames.pkl', 'a+') as f:
-        logging.info("Storing username {}".format(account['username']))
-        logging.info(account)
-        pickle.dump(account, f, pickle.HIGHEST_PROTOCOL)
+    with open(ASSET_DIR + '/usernames.pkl', 'ab') as f:
+        logging.info("Storing username {}".format(str(account['username'])))
+        logging.info(str(account))
+        pickle.dump(str(account), f, pickle.HIGHEST_PROTOCOL)
